6af9c4542851b85699038ef0b758e69ebe451fc2
Ole Kodehode <ole.Kodehode@gmail.com>
2025-07-20 13:23:54 +0200

    Added dynamic removal of confetti elements, tweaking of fall duration

--- script.js
+++ script.js
@@ -6,14 +6,29 @@ function spawnConfetti() {
     const confetti = document.createElement("article");
     confetti.classList.add("confetti_piece");
     confetti.style.left = `${Math.ceil(Math.random() * 100)}%`;
-    confetti.style.setProperty("--fall_duration", `${Math.random() * 3 + 3}s`);
+    confetti.style.setProperty(
+      "--fall_duration",
+      `${Math.random() * 3 + 1.5}s`
+    );
     confetti.style.setProperty("--confetti_color", getRandomColor());
     confettiWrapper.appendChild(confetti);
+    confetti.addEventListener("animationend", () => {
+      confetti.remove();
+    });
   }
-  document.querySelector(".confetti_button").removeAttribute("onclick");
 }
 
 function getRandomColor() {
-  const colors = ["#ff6347", "#ffa500", "#32cd32", "#1e90ff", "#ff69b4"];
+  const colors = [
+    "#e71818ff",
+    "#ffa500",
+    "#32cd32",
+    "#177ee4ff",
+    "#df3e8eff",
+    "#10e3ffff",
+    "#590ed1ff",
+    "#ffffff",
+    "#fffb01ff",
+  ];
   return colors[Math.floor(Math.random() * colors.length)];
 }
